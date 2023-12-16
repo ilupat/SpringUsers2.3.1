b83eef8e357536b63094cb921201e97b0129199b
Andrey <147320074+ilupat@users.noreply.github.com>
2023-12-16 18:42:44 +0300

    Initial commit

---
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -0,0 +1,40 @@
+package web.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.model.User;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("from User").getResultList();
+    }
+
+    @Override
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public void delete(User user) {
+        entityManager.remove(entityManager.find(User.class, user.getId()));
+    }
+
+    @Override
+    public void edit(User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public User getById(long id) {
+        return entityManager.find(User.class, id);
+    }
+}
